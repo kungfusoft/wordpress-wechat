0f39b93d39a363e2c70e53cb516cde2fad4bb233
Younger Shen <younger.x.shen@gmail.com>
2019-04-19 15:30:05 +0800

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wordpress Wechat 
+
+wordpress wechat is a wordpress plugin for wechat application development. it implements the major functions of wechat provides.
+
+the purpos of this piece plugin is to make wechat development in wordpress env much more **easy** and **fast**.
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WordPress Wechat
+Plugin URI: https://github.com/youngershen/wordpress-wechat
+Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from spam</strong>. It keeps your site protected even while you sleep. To get started: activate the Akismet plugin and then go to your Akismet Settings page to set up your API key.
+Version: 0.1
+Author: Younger Shen
+Author URI: https://www.punkcoder.cn
+License: MIT
+Text Domain: wordpress-wechat
+*/
