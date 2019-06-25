5f0ba51fad026c43f1152df781005767aed34dc3
Younger Shen <younger.x.shen@gmail.com>
2019-06-25 11:16:55 +0800

    Fix: fix typo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WordPress Wechat
 Plugin URI: https://github.com/youngershen/wordpress-wechat
-Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from spam</strong>. It keeps your site protected even while you sleep. To get started: activate the Akismet plugin and then go to your Akismet Settings page to set up your API key.
+Description: Wordpress Wechat is a wechat development platform, easy to use and easy to management.
 Version: 0.1
 Author: Younger Shen
 Author URI: https://www.punkcoder.cn
---
+++ utils.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * PROJECT : wordpress wechat
+ * FILE    : utils.php
+ * TIME    : 2019/5/8 9:45
+ * AUTHOR  : Younger Shen
+ * EMAIL   : younger.x.shen@gmail.com
+ * PHONE   : 13811754531
+ * WECHAT  : 13811754531
+ * WEBSIT  : https://www.punkcoder.cn
+ */
+
